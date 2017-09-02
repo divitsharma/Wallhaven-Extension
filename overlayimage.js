d15b4d4883ea3aea8bdeaabbd91734b2fdcfb8f8
MEPC\Divit <dsharma5095@gmail.com>
2017-09-02 19:19:40 -0400

    Minor changes

--- overlayimage.js
+++ overlayimage.js
@@ -1,10 +1,9 @@
 var desktopImg = new Image();
 var wall = document.getElementById("wallpaper");
 desktopImg.onload = function () {
-    wall.insertAdjacentElement("afterend",desktopImg);
+    wall.insertAdjacentElement("beforebegin",desktopImg);
 };
 var imgURL = chrome.extension.getURL("images/trans_desktop.png");
 desktopImg.src = imgURL;
 desktopImg.width = wall.width;
 desktopImg.height = wall.height;
-desktopImg.id = "desktop";
